c3a18d18a02298b8e735badc0e9fbd4c63e365cb
sergio4253 <perro1234yleon123456@gmail.com>
2026-07-09 16:20:50 -0500

    "Reto 1 – Mi primera presentación en GitHub"
    Sergio Romero

---
+++ # presentacion.py
@@ -0,0 +1,10 @@
+nombre = "sergio romero"
+grado = "1101"
+colegio = "Francisco Jose de Caldas"
+lenguaje = "Python 🐍"
+print("=" * 45)
+print(f" Hola GitHub! Soy {nombre}")
+print(f" Grado: {grado} | Institución: {colegio}")
+print(f" Aprendiendo: {lenguaje}")
+print(f" ¡Este es mi primer commit! ")
+print("=" * 45)
